5df5d9f2fc81de24e0a1068f905942c38f34f0e8
Bret Weinraub <bret@ckuru.com>
2010-07-23 04:15:40 +0000

    Latest updates

--- generators/meteor/meteor_generator.rb
+++ generators/meteor/meteor_generator.rb
@@ -1,8 +1,30 @@
-class MeteorGenerator < Rails::Generator::NamedBase
+class MeteorGenerator < Rails::Generator::Base
+
+  def recursive_copy(m,base,root=nil)
+    Dir.new("#{base}").each do |dir|
+      if root
+        root_text = "#{root}/"
+        new_root = "#{root}/#{dir}"
+      else
+        root_text = ""
+        new_root = dir
+      end
+      if File.directory? "#{base}/#{dir}"
+        next if dir == "." or dir == ".."
+        m.directory "#{root_text}#{dir}"
+        
+        recursive_copy(m,"#{base}/#{dir}",new_root)
+      else
+        m.file "#{root_text}#{dir}", "#{root_text}#{dir}"
+      end
+    end
+  end
+      
   def manifest
     record do |m|
-      # m.directory "lib"
-      # m.template 'README', "README"
+
+      recursive_copy(m,File.expand_path("../templates", __FILE__))
+      
     end
   end
 end
