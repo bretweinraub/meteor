ec7f324d1a73709e41049b8f7b12acde4a58036d
Tyler Lesmann <redhatcat@gmail.com>
2010-08-09 16:32:21 -0600

    XSS vulnerability fix

--- lib/meteor_crud/named_cell_crud.rb
+++ lib/meteor_crud/named_cell_crud.rb
@@ -2,13 +2,13 @@ module NamedCellCrud
 
   include MeteorCrudBase
 
-  ################################################################################ 
+  ################################################################################
 
   def named_cell_inplace_edit
-    
+
     spec_name = params['spec']
     raise "cannot find meteor spec for #{spec_name}" unless spec = meteor_specs[spec_name]
-    
+
     raise 'cannot determine object class' unless _klass = params['object_class']
     raise "no such class #{_klass}" unless klass = self.class.const_get(_klass)
 
@@ -17,11 +17,11 @@ module NamedCellCrud
 
     cell_name = params['cell_name']
     value = params['value']
-    
+
     object.send("#{cell_name}=",value)
     object.save!
     object.reload
-    
-    render :layout => false, :text => object.send(cell_name.to_sym).to_s
+
+    render :inline => CGI.escapeHTML(object.send(cell_name.to_sym).to_s)
   end
 end
