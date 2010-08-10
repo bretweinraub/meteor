b3eb72436bedb0308bbc351c1a8cec5628e4ec20
Tyler Lesmann <redhatcat@gmail.com>
2010-08-10 09:41:20 -0600

    Helper to replace blank cells with '-'.

--- lib/meteor_helper.rb
+++ lib/meteor_helper.rb
@@ -2,5 +2,17 @@ module MeteorHelper
   def option_equal(val1,val2)
     val1.to_s == val2.to_s
   end
+
+  def replace_blank(s, substitute='-')
+    if not s.kind_of? String
+      s = s.to_s
+    end
+
+    if s.blank?
+      s = substitute
+    end
+
+    s
+  end
 end
 
