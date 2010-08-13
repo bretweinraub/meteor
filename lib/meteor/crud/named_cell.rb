5958afb457b07457d5f62f98e298b7f9b6db0fbd
Tyler Lesmann <redhatcat@gmail.com>
2010-08-13 10:34:58 -0600

    Altered named cell crud of consistency.
    Now both Meteor and NamedCell cruds only require the controller to have
    one method, method_spec.  This method is expected to take a hash, which
    only receives a :name at this point in time.  It is expected to return
    some instance that is a kind_of? SpecBase.

--- lib/meteor/crud/named_cell.rb
+++ lib/meteor/crud/named_cell.rb
@@ -8,7 +8,7 @@ module Meteor
         cell_name = params['cell_name']
         spec_name = params['spec']
 
-        raise "cannot find meteor spec for #{spec_name}" unless spec = meteor_specs[spec_name]
+        raise "cannot find meteor spec for #{spec_name}" unless spec = meteor_spec(:name => spec_name)
         raise 'cannot determine object class' unless _klass = params['object_class']
         raise "no such class #{_klass}" unless klass = self.class.const_get(_klass)
         raise "cannot load object of type #{_klass} with id #{id}" unless object = klass.find(id)
