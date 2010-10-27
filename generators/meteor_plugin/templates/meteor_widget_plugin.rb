575fdc3027b6dfab7ddef2aa839e5b98925d3f82
bretweinraub <bret@ckuru.com>
2010-10-27 03:59:58 +0000

    fix pathing in this generated file

--- generators/meteor_plugin/templates/meteor_widget_plugin.rb
+++ generators/meteor_plugin/templates/meteor_widget_plugin.rb
@@ -5,7 +5,7 @@ module Meteor
         def plugin_name
           # XXX - I assume this will break down under heavy load; then name of the plugin this widget is found in.
           # This helps determine the render path for the template.
-          File.dirname(__FILE__).split(/\//).last
+          File.expand_path(File.join(File.dirname(__FILE__),'..','..','..','..')).split(/\//).last
         end
       end
     end
